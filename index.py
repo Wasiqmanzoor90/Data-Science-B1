6ce33725fb7be129a8e7fd3717e68cf5eede53af
Wasiqmanzoor90 <wasiqmanzoor90@gmail.com>
2025-12-11 16:01:43 +0530

    b1

---
+++ index.py
@@ -0,0 +1,31 @@
+# a ='umer'
+# print(a)
+
+# b ='hanan'
+# print(b)
+
+# c ='ubaid'
+# print(c)
+
+# d ='ali'
+# print(d)
+
+# e ='wasiq'
+# print(e)
+
+
+# abc =['wasiq', 3.5, True, 'ubaid'] #we have created an list
+# abc[0]='burhan'
+# print(abc)
+
+
+#tuple
+
+# thistuple = ('umer',5.6,'hanan',True,'hanan')
+
+# print(thistuple)
+
+thisset ={'umer',3.4,True,'burhan','umer'}
+thisset.add('ali')
+thisset.remove('burhan')
+print(thisset)
