29eea45f0b952683503babc4b54b5b25d30961c8
Wasiqmanzoor90 <wasiqmanzoor90@gmail.com>
2025-12-12 16:03:53 +0530

    v2

--- index.py
+++ index.py
@@ -15,17 +15,40 @@
 
 
 # abc =['wasiq', 3.5, True, 'ubaid'] #we have created an list
+# abc.append('umer') # we have added an element in the list using append function
+# abc.remove(3.5) # we have removed an element from the list using remove function
+# abc.insert(1,'hanan') # we have inserted an element at index 1 using insert function
+# abc.pop(4) # we have removed the last element from the list using pop function
 # abc[0]='burhan'
 # print(abc)
 
 
 #tuple
-
+# So Basically Tuple is same as list but the only difference is that tuple is immutable means we cant change the value of tuple once it is created
 # thistuple = ('umer',5.6,'hanan',True,'hanan')
-
+# thistuple[0]='musaib'
 # print(thistuple)
 
-thisset ={'umer',3.4,True,'burhan','umer'}
-thisset.add('ali')
-thisset.remove('burhan')
-print(thisset)
+
+#set
+# So Basically Set is a collection of unordered and unindexed elements
+# We can not access the elements of set by referring to an index because sets are unordered the items has no defined order
+# Sets are mutable means we can add or remove items from it but we can not change the items of set
+# thisset ={'umer',3.4,True,'burhan','umer'}
+# thisset.add('ali')
+# thisset.remove('burhan')
+# print(thisset)
+
+#slicing
+# thelist=['wasiq','ubaid',3.5,True,'hanan']
+# print(thelist[1:5])
+
+#dictionary
+# thisdict = {
+#     'name': 'umer',
+#     'age': 25,
+#     'gender': 'male',
+#     'sex':'male'
+# }
+# thisdict['age']=26
+# print(thisdict)
