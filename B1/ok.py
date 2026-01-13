e027dbf37d981562f3b198ce80816743e6265a1d
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-01-13 22:17:24 +0530

    Clean setup from Windows (B1 + B2)

---
+++ B1/ok.py
@@ -0,0 +1,50 @@
+# class calculator:
+  
+#   def __init__(self):
+#     print("im from Calculator")
+
+#     self.__add(12,12)
+#   def __add(self,a,b):
+#     print(a+b)
+
+#   def sub(self,a,b):
+#     print(a-b)
+    
+    
+# cal=calculator()
+
+# cal.sub(12,23)
+
+
+
+
+
+
+
+
+# class Calco:
+  
+#   def __init__(self): 
+#     print("Hello I'm from Constructor")
+#     self.__add(10,12)
+    
+#   def __add(self,a,b):
+#     print(a+b)
+# cal = Calco()
+
+
+class ok:
+    def __init__(self):
+        print("Hello i m from server")
+        self.__hello()
+    def __hello(self):
+        print("Hello World!")
+    
+    def _bye(self):
+        print("Bye world") 
+        
+        
+o = ok()
+# o.__hello()
+o._bye()
+
