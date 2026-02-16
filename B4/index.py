5b19b9330f8074024f093846e231f0f1e64e8e11
Wasiq Manzoor <wasiqmanzoor90@gmail.com>
2026-02-16 15:39:00 +0530

    j1

--- B4/index.py
+++ B4/index.py
@@ -4,28 +4,67 @@
 
 #Arthmatic opertors
 
-#Addition
-a = 30
-b =12
-print(a+b)
+# #Addition
+# a = 30
+# b =12
+# print(a+b)
 
 
-#subtraction
-s1 =  15
-s2 = 9
-print(s1-s2)
+# #subtraction
+# s1 =  15
+# s2 = 9
+# print(s1-s2)
 
-#multiplication
-m1 = 2
-m2 = 12
-print(m1*m2)
+# #multiplication
+# m1 = 2
+# m2 = 12
+# print(m1*m2)
 
-#divide
-d1 = 10
-d2 = 5
-print(d1/d2)
+# #divide
+# d1 = 10
+# d2 = 5
+# print(d1/d2)
 
-#Modules
-mo1 = 10
-mo22 = 5
-print(d1%d2)
+# #Modules
+# mo1 = 10
+# mo22 = 5
+# print(d1%d2)
+
+
+
+#Comparison Operator
+# a = 10
+# b =12
+# print(a == b)
+
+#Not Equal to
+# a = 10
+# b=10
+# print(a!=b)
+
+# #greater than
+# a = 12
+# print(a>18)
+
+# #less than
+# a = 12
+# print(a<18)
+
+#greater equal
+# a = 18
+# print(a>=18)
+
+#less equal
+# a = 12
+# print(a<=12)
+
+# a = 20
+# b = 25
+# print(a == b)
+
+
+# a = 20
+# b = 25
+# print(a!=b)
+
+
